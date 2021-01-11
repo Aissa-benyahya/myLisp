4e0f0a3438447a2ef0caef1ea2357b06f2e30df4
Aissa Ben yahya <ai.benyahya@edu.ac.umi.ma>
2021-01-11 22:17:59 +0100

    Alang first commitment

---
+++ language.md
@@ -0,0 +1,17 @@
+what is language?
+----------------
+A formal language consists of words whose letters are taken form an alphabet and are well-formed according to a specific set of rules.
+
+
+what is alphabet?
+-----------------
+The alphabet of a formal language consist of synbols, letters, or tokens that concatenate into strings of language.
+
+Grammer
+-------
+DESCRIBES how to form strings from a language's alphabet that are valid according to the language's syntax. A grammer does not describe the meaning of the strings or what can be done with then in whatever context only their form.
+
+
+product rules:
+-------------
+is a rewrite rule specifying a symbol substitution that can be recursively performed to generate new symbol sequances.
---
+++ parsing.c
@@ -0,0 +1,199 @@
+// ========== I'll build my own Lisp [2020-08-10] =====
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <editline/readline.h>
+//#include <editline/history.h>
+#include "mpc/mpc.h"
+
+static char buffer[2048];
+
+#ifdef _WIN32
+
+#include <string.h>
+// Artificial readline
+char *readline(char *prompt)
+{
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+
+    char *cpy = malloc(strlen(buffer)+1);
+    
+    strcpy(cpy, buffer);
+
+    cpy[strlen(cpy) - 1] = '0';
+    
+    return cpy;
+}
+
+void add_to_history(char *unused){}
+
+#else
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+#endif
+
+// Evaluation of user input
+int evaluation(mpc_ast_t* t)
+{
+  int result = 0;
+  int pos = 1;
+  int count = 0;
+  char* op = t->children[1]->contents;
+  switch(op[0])
+  {
+    case '+':
+      for(int i = 2; i < t->children_num-1; i++)
+      {
+        if(!strcmp(t->children[i]->tag, "expr|>"))
+        {
+          int res = evaluation(t->children[i]);
+          result += res;
+        }else{
+          result += atoi(t->children[i]->contents);
+        }
+      }
+      break;
+    case '-':
+      for(int i = 2; i < t->children_num-1; i++)
+      {
+        if(!strcmp(t->children[i]->tag, "expr|>"))
+        {
+          printf("hi inside strcmp\n");
+          int res = evaluation(t->children[i]);
+          if(count == 0)
+          {
+            result = res;
+            count++;
+          }
+          else
+          {
+            result -= res;
+          }
+        }else{
+          if(count == 0){
+            result = atoi(t->children[i]->contents);
+            count++;
+          }
+          else
+            result -= atoi(t->children[i]->contents);
+        }
+      }
+      break;
+    case '*':  
+      for(int i = 2; i < t->children_num-1; i++)
+      {
+        if(!strcmp(t->children[i]->tag, "expr|>"))
+        {
+          printf("hi inside strcmp\n");
+          int res = evaluation(t->children[i]);
+          pos *= res;
+        }else{
+          pos *= atoi(t->children[i]->contents);
+        }
+      }
+      result = pos;
+      break;
+    case '/':
+      for(int i = 2; i < t->children_num-1; i++)
+      {
+        if(!strcmp(t->children[i]->tag, "expr|>"))
+        {
+          printf("hi inside strcmp\n");
+          int res = evaluation(t->children[i]);
+          if(count == 0)
+          {
+            result = res;
+            count++;
+          }
+          else
+          {
+            result /= res;
+          }
+        }else{
+          if(count == 0)
+          {
+            result = atoi(t->children[i]->contents);
+            count++;
+          }
+          else
+          {
+            result /= atoi(t->children[i]->contents); 
+          }
+        }
+      }
+      break;
+    default:
+      printf("I can't do this operation yet!!\n");
+  }
+
+  return result;
+}
+// Interactive prompt
+int main(int argc, char **argv)
+{
+  // create a parser
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  // Define them with the folling language
+  mpca_lang(MPCA_LANG_DEFAULT,
+      "                                                    \
+        number    : /-?[0-9]+/ ;                           \
+        operator  : '+' | '-' | '*' | '/' ;                \
+        expr      : <number> | '(' <operator> <expr>+ ')' ; \
+        lispy     : /^/ <operator> <expr>+ /$/ ;           \
+      ",
+      Number, Operator, Expr, Lispy);
+
+    puts("Alang Version 1.0");
+    puts("author: B. Aissa");
+    puts("Type clear to clear the screen");
+    puts("Press Ctrl+c to Exit");
+    while(1){
+
+        // Output our prompt
+        char *input = readline("lisp> ");
+        if(!strcmp(input, "clear"))
+        {
+          system("clear");
+          input = readline("lisp> ");
+        }
+        
+
+        // Add buffer to history
+        add_history(input);
+
+        // Attempt to parse user input
+        mpc_result_t r;
+        int response = 0;
+        if(mpc_parse("<stdin>", input, Lispy, &r))
+        {
+          /* On Success print the AST */ 
+          mpc_ast_t* node = r.output;
+
+          //Evaluation of the user input
+          response = evaluation(node);
+          printf("--------------------------------\n");
+          printf("result = %d\n",response);
+          printf("--------------------------------\n");
+
+          // A parsing tree 
+          mpc_ast_print(r.output);
+          mpc_ast_delete(r.output);
+        }else{
+          /* Otherwise print the error */
+          mpc_err_print(r.error);
+          mpc_err_delete(r.error);
+        }
+
+	      // free retrived buffer
+	      free(input);
+    }
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    return 0;
+}
